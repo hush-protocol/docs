0f3afb7cb16db21c8450211a263ebb2a76770baf
dipanshuhappy <dipanshuhappy@gmail.com>
2024-09-09 15:46:54 +0530

    chore: Update production URL in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
   
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://hush-protocol.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/docs/',
