ca2c087fb41685346e70d1b5d28fec9e0ae72b5d
dipanshuhappy <dipanshuhappy@gmail.com>
2024-09-09 15:17:15 +0530

    chore: Update baseUrl to '/docs/' in docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -12,13 +12,13 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'hush-protocol', // Usually your GitHub org/user name.
   projectName: 'docs', // Usually your repo name.
-  deploymentBranch: 'main',
+  deploymentBranch: 'gh-pages',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
